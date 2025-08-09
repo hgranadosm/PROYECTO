3c76d1ba7a479780b918da47fe9729ad57218ff8
hgranadosm <hgranadosm@ucenfotec.ac.cr>
2025-08-09 12:26:45 -0600

    cambios servicios

--- js/servicios/buscadorFoto.js
+++ js/servicios/buscadorFoto.js
@@ -5,46 +5,46 @@ document.getElementById('menuOpciones').addEventListener('change', galeria);
 function galeria () {
     var opciones = document.getElementById('menuOpciones').value;
     switch(opciones){
-        case "carnes":
-            document.querySelector('.galeriaCarnes').style.display = "block"
-            document.querySelector('.galeriaPizzas').style.display = "none"
-            document.querySelector('.galeriaRapida').style.display = "none"
-            document.querySelector('.galeriaBebidas').style.display = "none"
-            document.querySelector('.galeriaPostres').style.display = "none"
+        case "costarricense":
+            document.querySelector('.galeriaCostarricense').style.display = "block"
+            document.querySelector('.galeriaItaliana').style.display = "none"
+            document.querySelector('.galeriaJaponesa').style.display = "none"
+            document.querySelector('.galeriaMexicana').style.display = "none"
+            document.querySelector('.galeriaColombiana').style.display = "none"
             break;
-        case "pizzas":
-            document.querySelector('.galeriaCarnes').style.display = "none"
-            document.querySelector('.galeriaPizzas').style.display = "block"
-            document.querySelector('.galeriaRapida').style.display = "none"
-            document.querySelector('.galeriaBebidas').style.display = "none"
-            document.querySelector('.galeriaPostres').style.display = "none"
+        case "italiana":
+            document.querySelector('.galeriaCostarricense').style.display = "none"
+            document.querySelector('.galeriaItaliana').style.display = "block"
+            document.querySelector('.galeriaJaponesa').style.display = "none"
+            document.querySelector('.galeriaMexicana').style.display = "none"
+            document.querySelector('.galeriaColombiana').style.display = "none"
             break;
-        case "rapida":
-            document.querySelector('.galeriaCarnes').style.display = "none"
-            document.querySelector('.galeriaPizzas').style.display = "none"
-            document.querySelector('.galeriaRapida').style.display = "block"
-            document.querySelector('.galeriaBebidas').style.display = "none"
-            document.querySelector('.galeriaPostres').style.display = "none"
+        case "japonesa":
+            document.querySelector('.galeriaCostarricense').style.display = "none"
+            document.querySelector('.galeriaItaliana').style.display = "none"
+            document.querySelector('.galeriaJaponesa').style.display = "block"
+            document.querySelector('.galeriaMexicana').style.display = "none"
+            document.querySelector('.galeriaColombiana').style.display = "none"
             break;
-        case "bebidas":
-            document.querySelector('.galeriaCarnes').style.display = "none"
-            document.querySelector('.galeriaPizzas').style.display = "none"
-            document.querySelector('.galeriaRapida').style.display = "none"
-            document.querySelector('.galeriaBebidas').style.display = "block"
-            document.querySelector('.galeriaPostres').style.display = "none"
+        case "mexicana":
+            document.querySelector('.galeriaCostarricense').style.display = "none"
+            document.querySelector('.galeriaItaliana').style.display = "none"
+            document.querySelector('.galeriaJaponesa').style.display = "none"
+            document.querySelector('.galeriaMexicana').style.display = "block"
+            document.querySelector('.galeriaColombiana').style.display = "none"
             break;
-        case "postres":
-            document.querySelector('.galeriaCarnes').style.display = "none"
-            document.querySelector('.galeriaPizzas').style.display = "none"
-            document.querySelector('.galeriaRapida').style.display = "none"
-            document.querySelector('.galeriaBebidas').style.display = "none"
-            document.querySelector('.galeriaPostres').style.display = "block"
+        case "colombiana":
+            document.querySelector('.galeriaCostarricense').style.display = "none"
+            document.querySelector('.galeriaItaliana').style.display = "none"
+            document.querySelector('.galeriaJaponesa').style.display = "none"
+            document.querySelector('.galeriaMexicana').style.display = "none"
+            document.querySelector('.galeriaColombiana').style.display = "block"
             break;
         default:
-            document.querySelector('.galeriaCarnes').style.display = "none"
-            document.querySelector('.galeriaPizzas').style.display = "none"
-            document.querySelector('.galeriaRapida').style.display = "none"
-            document.querySelector('.galeriaBebidas').style.display = "none"
-            document.querySelector('.galeriaPostres').style.display = "none"
+            document.querySelector('.galeriaCostarricense').style.display = "none"
+            document.querySelector('.galeriaItaliana').style.display = "none"
+            document.querySelector('.galeriaJaponesa').style.display = "none"
+            document.querySelector('.galeriaMexicana').style.display = "none"
+            document.querySelector('.galeriaColombiana').style.display = "none"
     }
 }
--- js/servicios/combo1.js
+++ js/servicios/combo1.js
@@ -1,23 +1,46 @@
 window.onload = function() {
-    document.getElementById("output-img").innerHTML = "<img src='img/reto4/perros.jpg' class='img-fluid rounded' alt='perro' />";
+    const defaultImg = "img/servicios/default.jpg";
+    document.getElementById("output-img").innerHTML = `<img src='${defaultImg}' class='img-fluid rounded' alt='servicio default' />`;
+    document.getElementById("output-txt").innerHTML = "";
 
-    document.getElementById("razas").onchange = function(e) {
-        let n = e.target.value;
-        let imagen = "<img src='img/reto4/" + n + ".jpg' class='img-fluid img-thumbnail rounded' alt='estadio' />";
-        let text = ["Labrador Retriever",
-            "Pastor Alemán",
-            "Golden Retriever",
-            "Husky Siberiano",
-            "Chihuahua",
-        ];
-        document.getElementById("output-img").innerHTML = imagen;
-        document.getElementById("output-txt").innerHTML = text[n-1];
+    const serviciosData = {
+        alojamiento: {
+            img: "img/servicios/1.jpg",
+            txt: "Alojamiento en habitaciones confortables"
+        },
+        restaurante: {
+            img: "img/servicios/2.jpg",
+            txt: "Restaurante con comida típica de la región"
+        },
+        spa: {
+            img: "img/servicios/3.jpg",
+            txt: "Spa con masajes relajantes"
+        },
+        excursiones: {
+            img: "img/servicios/4.jpg",
+            txt: "Excursiones y senderismo guiado"
+        },
+        jacuzzi: {
+            img: "img/servicios/5.jpg",
+            txt: "Jacuzzi al aire libre"
+        }
     };
-};
 
+    document.getElementById("servicios").onchange = function(e) {
+        let key = e.target.value;
+        if(serviciosData[key]) {
+            let imagen = `<img src='${serviciosData[key].img}' class='img-fluid rounded' alt='servicio' />`;
+            document.getElementById("output-img").innerHTML = imagen;
+            document.getElementById("output-txt").innerHTML = serviciosData[key].txt;
+        } else {
+            document.getElementById("output-img").innerHTML = `<img src='${defaultImg}' class='img-fluid rounded' alt='servicio default' />`;
+            document.getElementById("output-txt").innerHTML = "";
+        }
+    };
 
-document.getElementById("btn-clean").onclick = function(){
-    document.getElementById("output-img").innerHTML = "<img src='img/reto4/perros.jpg' class='img-fluid rounded' alt='perro' />";
-     document.getElementById("output-txt").innerHTML = "";
-     document.getElementById("razas").value = "";
+    document.getElementById("btn-clean").onclick = function(){
+        document.getElementById("output-img").innerHTML = `<img src='${defaultImg}' class='img-fluid rounded' alt='servicio default' />`;
+        document.getElementById("output-txt").innerHTML = "";
+        document.getElementById("servicios").value = "";
+    }
 }
