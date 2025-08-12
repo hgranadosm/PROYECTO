3fe4d34f6104ba943357a04ce2d404e15534d2cb
hgranadosm <hgranadosm@ucenfotec.ac.cr>
2025-08-11 20:22:45 -0600

    cambio

--- js/login.js
+++ js/login.js
@@ -1,12 +1,24 @@
 document.getElementById("btn-login").addEventListener("click", login); 
 
 function validation_alert(ptext) {
-    swal.fire({
-        title: "Asegúrate de que todos los campos estén completos",
-        text: ptext,
+    Swal.fire({
+        icon: "error",
+        title: '',
+        html: `<div style="background-color: transparent; color: #333; border-radius: 8px; padding: 18px; text-align: center; font-size: 1rem; font-weight: 500;">
+                <h3 style='font-size:1.3rem; font-weight:600; margin-bottom:18px;'>Asegúrate de que todos los campos estén completos</h3>
+                <iframe width="320" height="240" frameborder="0" src="https://lottie.host/embed/889f8969-bef6-45eb-a988-15965bfa28c5/I8XhTMjfrO.lottie"></iframe>
+                <span style='display:block; margin-bottom:12px;'>${ptext}</span>
+            </div>`,
+        background: '#f1ebe3',
+        color: '#333',
+        customClass: {
+            popup: 'swal2-custom-popup',
+            title: 'swal2-custom-title',
+            confirmButton: 'swal2-custom-confirm',
+        },
+        showConfirmButton: true,
         confirmButtonText: "Intentar nuevamente",
         confirmButtonColor: "#8b0000",
-        html: '<iframe width="320" height="240" frameborder="0" src="https://lottie.host/embed/889f8969-bef6-45eb-a988-15965bfa28c5/I8XhTMjfrO.lottie"></iframe> <br><p>' + ptext + " </p>", 
     });
 }
 
@@ -34,15 +46,23 @@ function login() {
     if (error_count == 0) {  
         if (user_input == username && pass_input == password) {
             Swal.fire({
-                title: "Acceso correcto",
-                showConfirmButton: false,
-                customClass: {                 
-                    title: 'formatos1',                      
+                title: '',
+                html: `<div style="background-color: transparent; color: #333; border-radius: 8px; padding: 18px; text-align: center; font-size: 1rem; font-weight: 500;">
+                        <h3 style='font-size:1.3rem; font-weight:600; margin-bottom:18px;'>Acceso correcto</h3>
+                        <iframe width="320" height="240" frameborder="0" src="https://lottie.host/embed/6cd7f08c-451a-459e-a3c0-f97a03e636af/IEcz3avkKm.lottie"></iframe>
+                        <span style='display:block; margin-bottom:12px;'>Esperar un momento...</span>
+                    </div>`,
+                background: '#f1ebe3',
+                color: '#333',
+                customClass: {
+                    popup: 'swal2-custom-popup',
+                    title: 'swal2-custom-title',
+                    confirmButton: 'swal2-custom-confirm',
                 },
+                showConfirmButton: false,
                 timer: 4000,
-                html: '<iframe width="320" height="240" frameborder="0" src="https://lottie.host/embed/6cd7f08c-451a-459e-a3c0-f97a03e636af/IEcz3avkKm.lottie"></iframe> <br><br><p>Esperar un momento...</p>',                
             }).then(() => {
-                window.location.href = "landing.html", "blank"; 
+                window.location.href = "landing.html"; 
             });
         } else {
             text = "Usuario o contraseña incorrecta."; 
